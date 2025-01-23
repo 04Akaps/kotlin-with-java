5688f043e8de9a93740f482ed390cae9e1c1ce19
jukas <jukas@wemade.com>
2025-01-23 17:55:39 +0900

    Feat: change routine logic

--- src/main/kotlin/service/CircuitService.kt
+++ src/main/kotlin/service/CircuitService.kt
@@ -18,6 +18,12 @@ class CircuitService(
     private  val httpClient: HttpClient
 ) {
 
+    private  val testUrls = listOf(
+        "http://localhost:7002/api/circuit/one",
+        "http://localhost:7002/api/circuit/two",
+        "http://localhost:7002/api/circuit/three",
+    )
+
     // 가장 간단한 코루틴
     suspend fun testRoutine() : String = coroutineScope {
 
@@ -37,109 +43,43 @@ class CircuitService(
     }
 
      fun testCircuitWithRoutine() : String  {
-         val one: Deferred<String?>
-         val two: Deferred<String?>
-         val three: Deferred<String?>
 
-         runBlocking {
+         // http calls
+         val deferredResults: List<Deferred<String?>> = runBlocking {
              supervisorScope {
-                 one = async {
-                     println("one start")
-                     try {
-                         val result = httpClient.get("http://localhost:7002/api/circuit/one").body<String>()
-                         println(result)
-                         result
-                     } catch (e: Exception) {
-                         println("Error in one: ${e.message}")
-                         null
-                     }
-                 }
-
-                 two = async {
-                     println("two start")
-                     try {
-                         val result = httpClient.get("http://localhost:7002/api/circuit/two").body<String>()
-                         println(result)
-                         result
-                     } catch (e: Exception) {
-                         println("Error in two: ${e.message}")
-                         null
-                     }
-                 }
-
-                 three = async {
-                     println("three start")
-                     try {
-                         val result = httpClient.get("http://localhost:7002/api/circuit/three").body<String>()
-                         println(result)
-                         result
-                     } catch (e: Exception) {
-                         println("Error in three: ${e.message}")
-                         null
+                 testUrls.map { url ->
+                     async {
+                         println("${url.split("/").last()} start")
+                         fetchResult(url)
                      }
                  }
              }
-
-             // 모든 작업 결과 대기 및 결합
-             val result = (one.await() ?: "one_failed") +
-                     (two.await() ?: "two_failed") +
-                     (three.await() ?: "three_failed")
-             println("Final Result: $result")
          }
 
-//        val one = async {
-//           val res:String = httpClient.get("http://localhost:7002/api/circuit/one").body<String>()
-//            println(res)
-//        }
-//
-//        val two = async {
-//            val res:String =  httpClient.get("http://localhost:7002/api/circuit/two").body<String>()
-//            println(res)
-//        }
-//
-//        val three = async {
-//            val res:String = httpClient.get("http://localhost:7002/api/circuit/three").body<String>()
-//            println(res)
-//        }
+         // wait result
+         val results = runBlocking {
+             deferredResults.map { it.await() ?: "${it::class.simpleName?.toLowerCase()}_failed" }
+         }
 
-        return ""
+         val finalResult = results.joinToString("")
+         println("Final Result: $finalResult")
+         return finalResult
     }
 
-
-
+    private suspend fun fetchResult(url: String): String? {
+        return try {
+            val result = httpClient.get(url).body<String>()
+            println(result)
+            result
+        } catch (e: Exception) {
+            println("Error in $url: ${e.message}")
+            null
+        }
+    }
 
     private fun breaker(key: CircuitCollector): CircuitBreaker {
         return circuitTemplate.get(key) ?: throw CustomException(ErrorCode.FailedToFindBreaker)
     }
 
 
-//
-//    fun callApi(api: CircuitCollector) {
-//        val circuitBreaker = circuitTemplate[api]
-//
-//        // CircuitBreaker가 null이 아닌지 확인하고 사용
-//        circuitBreaker?.let {
-//            // CircuitBreaker를 통해 API 호출을 감쌈
-//            val result = CircuitBreaker.decorateSupplier(it) {
-//                // 실제 외부 API 호출 코드
-//                externalApiCall(api)
-//            }
-//
-//            try {
-//                val response = result.get()  // API 응답 가져오기
-//                println("API 응답: $response")
-//            } catch (e: Throwable) {
-//                // 회로가 열려 있거나 실패한 경우 예외 처리
-//                println("API 호출 실패: ${e.message}")
-//            }
-//        } ?: run {
-//            println("해당 API에 대한 CircuitBreaker 설정이 없습니다.")
-//        }
-//    }
-//
-//    // 실제 외부 API 호출을 처리하는 메서드 (예시)
-//    private fun externalApiCall(api: CircuitCollector): String {
-//        // 외부 API 호출 로직
-//        return "API 응답: ${api.key}"  // 예시로 문자열 반환
-//    }
 }
